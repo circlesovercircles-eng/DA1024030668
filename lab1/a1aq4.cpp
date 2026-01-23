d306b5fc055ad378472f1c26f60d4e1c0c247929
circlesovercircle-eng <227041867+circlesovercircles-eng@users.noreply.github.com>
2026-01-23 11:41:12 +0530

    4th add

--- lab1/a1aq4.cpp
+++ lab1/a1aq4.cpp
@@ -1,5 +1,60 @@
-#include <iostream>
-#include <algorithm>
-#include <vector>
+#include<bits/stdc++.h>
+#include<vector>
+#include<climits>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n, k;
+    cin >> n >> k;
+
+    vector<int> A(n);
+    for (int i = 0; i < n; i++) cin >> A[i];
+
+    vector<pair<int,int>> vals;
+    for (int i = 0; i < n; i++)
+        vals.push_back({A[i], i});
 
 
+    sort(vals.begin(), vals.end(), greater<>());
+
+    vector<int> B(n, 0);          
+    long long answer = 0;
+    int ptr = 0;
+
+
+    for (int idx = 0; idx < n; idx++) {
+        int x = vals[idx].first;
+
+        while (ptr < n && vals[ptr].first > x) {
+            B[vals[ptr].second] = 1;
+            ptr++;
+        }
+
+
+        unordered_map<int,int> freq;
+        freq[0] = 1;
+
+        int prefix = 0;
+        bool found = false;
+
+        for (int i = 0; i < n; i++) {
+            prefix += B[i];
+            if (freq.count(prefix - k)) {
+                found = true;
+                break;
+            }
+            freq[prefix]++;
+        }
+
+        if (found) {
+            answer += x;
+        }
+    }
+
+    cout << answer << '\n';
+    return 0;
+}
